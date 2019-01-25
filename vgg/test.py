a452cf6cdc7ef2ef7eb944cddcabb8a6f32ab97b
xiszishu <xiszishu@gmail.com>
2019-01-24 23:16:33 -0800

    evaluate with topk and add progress bar

--- vgg/test.py
+++ vgg/test.py
@@ -4,6 +4,7 @@ from tensorflow.keras.applications.vgg16 import preprocess_input, decode_predict
 import numpy as np
 import tensorflow as tf
 import sys
+from progress.bar import IncrementalBar
 
 
 # This example tests a dog picture
@@ -27,20 +28,37 @@ def main(argv):
   #test_dog_picture()
   result_file = open("val.txt", "r")
   correct_num = 0
-  num_images = 50000
+  num_images = 10000
+  topk = 5
+  filename = tf.placeholder(tf.string, name="inputFile")
+  fileContent = tf.read_file(filename, name="loadFile")
+  image_file = tf.image.decode_jpeg(fileContent, channels=3, name="decodeJpeg")
+  resize_nearest_neighbor = tf.image.resize_images(image_file,
+                                                   size=[224,224],
+                                                   method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+  sess = tf.Session()
+  suffix = '%(index)d/%(max)d [%(elapsed)d / %(eta)d / %(eta_td)s]'
+  bar = IncrementalBar('Processing', max=num_images, suffix=suffix)
   for i in range(1, num_images + 1):
     img_file = "{}{}{:0>8d}{}".format(image_path, "ILSVRC2012_val_", i,
                                       ".JPEG")
-    img2 = image.load_img(img_file)
-    resized_images = img2.resize((224, 224))
-    x = image.img_to_array(resized_images)
+    feed_dict={filename: img_file}
+    with sess.as_default():
+      x = resize_nearest_neighbor.eval(feed_dict)
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
     features = model.predict(x)
-    result = np.argmax(features)
+    #result = np.argmax(features)
+    result = np.argsort(features)[0][-topk:]
     result_line = result_file.readline()
     correct_result = int(result_line.split()[1])
-    if (correct_result == result): correct_num += 1
+    #print(correct_result, result)
+    if (correct_result in result): correct_num += 1
+    #else: print(img_file)
+    bar.next()
+
+  bar.finish()
+  #resized_images.save("picture_resized.jpeg", "JPEG", optimize=True)
   print("Accuracy: {0:.2f}%".format(float(correct_num) / num_images * 100))
 
 
